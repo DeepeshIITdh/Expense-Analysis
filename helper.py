391813719b539d30d801f766e189d9a2a5edad8e
DeepeshIITdh <dsh.2065@gmail.com>
2024-06-24 11:53:09 +0530

    Adding helper functions file

---
+++ helper.py
@@ -0,0 +1,82 @@
+import pandas as pd
+from PyPDF2 import PdfReader
+import numpy as np
+
+
+def transaction_pdf_to_csv_phonepe(pdf_path):
+    # creating list for the columns of DataFrame
+    date = []
+    time = []
+    name_details = []
+    account_used = []
+    payment_type = []
+    amount = []
+
+    # creating the PdfReader Object
+    reader = PdfReader(pdf_path)
+
+    # extracting text from each page
+    for i in range(len(reader.pages)):
+        page = reader.pages[i]
+        text = page.extract_text()
+        text_list = text[:text.index('Page')].split('\n')
+
+        # only for the first page
+        if i == 0:
+            user = text_list[0]
+            time_interval = text_list[1]
+            text_list.remove(user)
+            text_list.remove(time_interval)
+
+        # extracting amount, name_details, payment_type column
+
+        for i, word in enumerate(text_list):
+            if ('₹' in word) or ('INR' in word) or ('Rs' in word):
+                # appending elements to their resp. lists
+                amount.append(word)
+                indi_name_detail = text_list[i + 1]
+                indi_payment_type = text_list[i - 1]
+                name_details.append(indi_name_detail)
+                payment_type.append(indi_payment_type)
+
+                # remove the extracted words from the text list
+                text_list.remove(word)
+                text_list.remove(indi_name_detail)
+                text_list.remove(indi_payment_type)
+
+        # extracting date, time, account_used column
+        for [i, word] in enumerate(text_list):
+            if ',' in word:
+                date.append(word)
+                time.append(text_list[i + 1])
+            elif 'X' * 5 in word:
+                account_used.append(word)
+
+    # creating the DataFrame
+    df = pd.DataFrame({
+        'Date': date,
+        'Time': time,
+        'name_details': name_details,
+        'Account_used': account_used,
+        'Type': payment_type,
+        'Amount': amount
+    })
+
+    df['Amount'] = df['Amount'].str.replace('₹', '').str.replace(',', '').astype(np.float64).round(0).astype(np.int64)
+    df['Date'] = pd.to_datetime(df['Date']).dt.date
+    df['Time'] = pd.to_datetime(df['Time'], format='%I:%M %p').dt.time
+
+    # converting dataframe to csv
+    df.to_csv('output.csv', index=False)
+
+
+def expense_over_timeline(csv_path):
+    df = pd.read_csv(csv_path)
+
+    # If the date is in 'yyyy/mm/dd' format and time is in 'HH:MM:SS' format
+    df['Datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'], format='%Y-%m-%d %H:%M:%S')
+
+    debit_df = df[(df['Type'] == 'DEBIT') | (df['Type'] == 'Debit')]
+    credit_df = df[(df['Type'] == 'CREDIT') | (df['Type'] == 'Credit')]
+
+    return debit_df, credit_df
